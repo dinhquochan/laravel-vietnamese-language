817e1b702fbb143ac229235040096419ce46a59f
Dinh Quoc Han <dinhquochan96@gmail.com>
2018-05-25 16:56:55 +0700

    Init commit

---
+++ auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Không tìm thấy thông tin đăng nhập hợp lệ.',
+    'throttle' => 'Đăng nhập sai quá nhiều lần. Vui lòng thử lại sau :seconds giây.',
+
+];
