c9dab131f70a26557d58affda8427e3903a35f21
Dinh Quoc Han <dinhquochan96@gmail.com>
2018-05-25 17:25:03 +0700

    Update Laravel 5.6

--- pagination.php
+++ pagination.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'previous' => '&laquo; Trước',
-    'next' => 'Sau &raquo;',
+    'previous'   => '&laquo; Trước',
+    'next'       => 'Sau &raquo;',
 
 ];
--- passwords.php
+++ passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Mật khẩu phải gồm 6 ký tự và khớp với phần xác nhận.',
-    'reset' => 'Mật khẩu đã được cập nhật!',
-    'sent' => 'Chúng tôi đã gửi cho bạn đường dẫn thay đổi mật khẩu!',
-    'token' => 'Mã xác nhận mật khẩu không hợp lệ.',
-    'user' => 'Không tìm thấy thành viên với địa chỉ email này.',
+    'password'   => 'Mật khẩu phải gồm 6 ký tự và khớp với phần xác nhận.',
+    'reset'      => 'Mật khẩu đã được cập nhật!',
+    'sent'       => 'Chúng tôi đã gửi cho bạn đường dẫn thay đổi mật khẩu!',
+    'token'      => 'Mã xác nhận mật khẩu không hợp lệ.',
+    'user'       => 'Không tìm thấy thành viên với địa chỉ email này.',
 
 ];
